33b6b4c4ce2076434fab4038ce2ca332288da41a
AdrianaMagdalena <128493793+AdrianaMagdalena@users.noreply.github.com>
2026-09-06 01:18:40 +0200

    TODO List - modules folder rename, move id generator to todo-item class

---
+++ src/modules/todo-item.ts
@@ -0,0 +1,50 @@
+/*
+for creating a todo item class & constructor
+without methods or logic behind it
+just to be able to instantiate a todo inside a list
+
+properties:
+MUST HAVE:
+- ID (id randomisation?)
+- category (optional, if not chosen - default to "general")
+- content
+
+NICE TO HAVE:
+- name & description (optional) instead of content
+- deadline
+- iteration (one-time or repeting todo?)
+- reminder alarm (how long before? multiple?)
+- enum with statuses (or other status handling)
+*/
+
+export default class TodoItem<T> {
+  private _id: string;
+  private _content: T;
+
+  constructor(content: T) {
+    this._id = this.generateTodoId(6);
+    this._content = content;
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get content(): T {
+    return this._content;
+  }
+
+  private generateTodoId(length: number): string {
+    // random ID generator idea from:
+    // https://www.webdevtutor.net/blog/typescript-get-random-id
+    const chars: string =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result: string = "";
+
+    for (let i = 0; i < length; i++) {
+      result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+
+    return result;
+  }
+}
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,4 @@
 /*
 For advances types and aliases
 */
+export type TodoContent = string | object;
